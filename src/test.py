5beebe490293161994f1667fe7223467484bca2d
Owner <owner@Owners-MacBook-Pro.local>
2024-01-08 08:32:41 -0500

    comments, logic

--- src/test.py
+++ src/test.py
@@ -3,22 +3,28 @@ from gsmarena_scraper.classes import GSMArenaPhone
 from gsmarena_scraper.classes.phone import GSMArenaPhoneBasic
 from gsmarena_scraper.utils import to_json, user_agents
 import time
-
+import os
+import json
 import random
 
-client = GSMArenaClient()
-# phone_list = client.list_phones()
+LIST_START = 0
+BLOCK_SIZE = 50
 
-# random.shuffle(phone_list)
-# with open("list.json", "w+") as f:
-#     f.write(to_json(phone_list))
+CLIENT = GSMArenaClient()
 
-import json
 phone_list = []
-with open("list.json", "r") as f:
-    data = json.load(f)
-    for phone_json in data:
-        phone_list.append(GSMArenaPhoneBasic(**phone_json))
+
+if os.path.exists("list.json"):
+    with open("list.json", "r") as f:
+        data = json.load(f)
+        for phone_json in data:
+            phone_list.append(GSMArenaPhoneBasic(**phone_json))
+
+else: 
+    phone_list = CLIENT.list_phones()
+    random.shuffle(phone_list)
+    with open("list.json", "w+") as f:
+        f.write(to_json(phone_list))
 
 
 print("Discovered ",len(phone_list), " devices")
@@ -26,10 +32,9 @@ print("Discovered ",len(phone_list), " devices")
 out = []
 failed_devices = []
 
-i = 10650
-block_size = 50
+i = LIST_START
 ua_index = 0
-client.headers.update({
+CLIENT.headers.update({
     "User-Agent": user_agents[ua_index]
 })
 while i < len(phone_list):
@@ -38,7 +43,7 @@ while i < len(phone_list):
     print(phone.url)
 
     # Get phone specs html
-    phone_res = client.get(phone.url)
+    phone_res = CLIENT.get(phone.url)
     try:
         phone_res.raise_for_status()
         phone_data = GSMArenaPhone.from_html_response(phone_res.content, basic_details=phone)
@@ -54,20 +59,20 @@ while i < len(phone_list):
         failed_devices.append(phone)
         continue
 
-    if not i % block_size or i == len(phone_list):
+    # If reached end of block or end of list, dump contents, clear containers
+    if not i % BLOCK_SIZE or i == len(phone_list):
         ua_index = len(user_agents) % (ua_index + 1)
 
-        client.headers.update({
+        CLIENT.headers.update({
             "User-Agent": user_agents[ua_index]
         })
 
         with open(f"found.{i}.json", "w+") as f:
             f.write(to_json(out))
-        
+        out = []
+
         with open(f"failed.{i}.json", "w+") as f:
             f.write(to_json(failed_devices))
-
-        out = []
         failed_devices=[]
 
         print(f"Block {i} complete, waiting 10 seconds...")
